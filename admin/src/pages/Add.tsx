587359110e5e125e327cc98bb10f9075f4a8ec3d
hungpn23 <hungpn23@gmail.com>
2024-10-04 22:26:49 +0700

    m

--- admin/src/pages/Add.tsx
+++ admin/src/pages/Add.tsx
@@ -29,9 +29,9 @@ export default function Add() {
         </div>
       </div>
 
-      <div>
-        <p>Product Name</p>
-        <input type="text" placeholder="Enter product name" required />
+      <div className="w-full">
+        <p className="mb-2">Product Name</p>
+        <input className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Enter product name" required />
       </div>
     </form>
   );
